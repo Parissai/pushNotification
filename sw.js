7e0742e2b10e460b29f0e3b166c9964778d0d463
Parissai <parissa.jamali@gmail.com>
2018-04-28 21:34:23 +0100

    handle notification click

--- sw.js
+++ sw.js
@@ -4,13 +4,23 @@ self.addEventListener('push', function (event) {
   console.log('[Service Worker] Push Received.');
   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-  const title = 'Push Codelab';
+  const title = 'Push Notification';
   const options = {
     body: 'Yay it works.',
     icon: '/icon.png',
     badge: '/badge.png',
-    sound: "/alarm.mp3"
+    sound: '/alarm.mp3'
   };
 
   event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', function (event) {
+  console.log('[Service Worker] Notification click Received.');
+
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow('https://github.com')
+  );
 });
