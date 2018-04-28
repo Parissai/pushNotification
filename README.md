b2092318f42ea4f02c00c6956d3194c20bc0ab84
Parissai <parissa.jamali@gmail.com>
2018-04-28 20:19:50 +0100

    Subscribe user

--- README.md
+++ README.md
@@ -8,9 +8,23 @@
 
 devTools > application > Service Workers > during development check "Update on Reload" checkbox
 
-### resources 
+## resources 
 
-The [PushManager.getSubscription()]() method of the PushManager interface retrieves an existing push subscription. It returns a Promise that resolves to a PushSubscription object containing details of an existing subscription. If no existing subscription exists, this resolves to a null value.
+__*[PushManager.subscribe()](https://developer.mozilla.org/en-US/docs/Web/API/PushManager/subscribe)*__ returns a Promise that resolves to a PushSubscription object containing details of a push subscription. A new push subscription is created if the current service worker does not have an existing subscription.
+```js
+​PushManager.subscribe(options).then(function(pushSubscription) { ... } );
+```
+**Parameters:**
+
+**options** (optional)
+
+An object containing optional configuration parameters. It can have the following properties:
+
+- **userVisibleOnly:** A boolean indicating that the returned push subscription will only be used for messages whose effect is made visible to the user.
+
+- **applicationServerKey:** A Base64-encoded DOMString or ArrayBuffer containing an ECDSA P-256 public key that the push server will use to authenticate your application server. If specified, all messages from your application server must use the VAPID authentication scheme, and include a JWT signed with the corresponding private key. This key IS NOT the same ECDH key that you use to encrypt the data. For more information, see "Using VAPID with WebPush".
+
+__*[PushManager.getSubscription()](https://developer.mozilla.org/en-US/docs/Web/API/PushManager/getSubscription)*__ method of the PushManager interface retrieves an existing push subscription. It returns a Promise that resolves to a PushSubscription object containing details of an existing subscription. If no existing subscription exists, this resolves to a null value.
 
 
-[Notification.Notification()](https://developer.mozilla.org/en-US/docs/Web/API/notification/Notification)
+__*[Notification.Notification()](https://developer.mozilla.org/en-US/docs/Web/API/notification/Notification)*__
--- main.js
+++ main.js
@@ -6,6 +6,21 @@ const pushButton = document.querySelector('.js-push-btn');
 let swRegistration = null;
 let isSubscribed = false;
 
+function urlB64ToUint8Array(base64String) {  // convert base 64 URL safe encoded to a UInt8Array
+  const padding = '='.repeat((4 - base64String.length % 4) % 4);
+  const base64 = (base64String + padding)
+    .replace(/\-/g, '+')
+    .replace(/_/g, '/');
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
 if ('serviceWorker' in navigator && 'PushManager' in window) { //check if service workers & push messaging is supported by the current browser. If it is, register sw.js
   console.log('Service Worker and Push is supported');
 
@@ -34,10 +49,59 @@ function updateBtn() {
   pushButton.disabled = false;
 }
 
+function updateSubscriptionOnServer(subscription) { // NOT HERE send subscription to a backend
+  // TODO: Send subscription to application server
+  console.log('SUBSCRIPTION: ', subscription);
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
+}
+
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey); // convert application server's public key, from base 64 URL safe encoded to a UInt8Array
+  swRegistration.pushManager.subscribe({
+    userVisibleOnly: true, // every time a push is sent, will show a notification 
+    applicationServerKey
+  })
+    .then(function (subscription) {
+      console.log('User is subscribed.');
+
+      updateSubscriptionOnServer(subscription);
+
+      isSubscribed = true;
+
+      updateBtn();  // re enable the button
+    })
+    .catch(function (err) {
+      console.log('Failed to subscribe the user: ', err);
+      updateBtn();  // re enable the button
+    });
+}
+
 function initializeUI() {
+
+  pushButton.addEventListener('click', function () { // toggle user subscription 
+    pushButton.disabled = true; // disable button so user can't click for the second time
+    if (isSubscribed) {
+      // unsubscribeUser();
+    } else {
+      subscribeUser();
+    }
+  });
+
   swRegistration.pushManager.getSubscription() // check if the user is currently subscribed..pushManager.getSubscription() return a Promise that resolves to a PushSubscription object containing details of an existing subscription
     .then(function (subscription) {
       isSubscribed = !(subscription === null);
+
+      updateSubscriptionOnServer(subscription);
 
       if (isSubscribed) {
         console.log('User IS subscribed.');
